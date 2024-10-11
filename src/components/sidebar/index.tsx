2052b9421281a1bcda71f39f2babf82e16792046
TreNgheDiCode <gabayan170@gmail.com>
2024-10-11 19:02:58 +0700

    Workspace Switcher

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import DottedSeparator from "../dottedSeparator";
 import SidebarNavigation from "./navigation";
+import WorkspaceSwitcher from "./workspaceSwitcher";
 
 export default function Sidebar() {
   return (
@@ -11,6 +12,8 @@ export default function Sidebar() {
       <Link href="/">
         <Image src="/logo.svg" alt="logo" width={164} height={48} />
       </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <SidebarNavigation />
     </aside>
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -5,66 +5,74 @@ import { sessionMiddleware } from "@/lib/sessionMiddleware";
 import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 import { AppwriteException, ID } from "node-appwrite";
 
-const app = new Hono().post(
-  "/",
-  zValidator("form", createWorkspaceSchema),
-  sessionMiddleware,
-  async (c) => {
-    const databases = c.get("databases");
-    const storage = c.get("storage");
-    const user = c.get("user");
+const app = new Hono()
+  .get("/", sessionMiddleware, async (c) => {
+    const database = c.get("databases");
 
-    const { name, image } = c.req.valid("form");
+    const workspaces = await database.listDocuments(DATABASE_ID, WORKSPACES_ID);
 
-    let uploadedImageUrl: string | undefined;
+    return c.json({ data: workspaces });
+  })
+  .post(
+    "/",
+    zValidator("form", createWorkspaceSchema),
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
 
-    try {
-      if (image instanceof File) {
-        const file = await storage.createFile(
-          IMAGES_BUCKET_ID,
-          ID.unique(),
-          image
-        );
+      const { name, image } = c.req.valid("form");
 
-        const arrayBuffer = await storage.getFilePreview(
-          IMAGES_BUCKET_ID,
-          file.$id
-        );
+      let uploadedImageUrl: string | undefined;
 
-        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
-          arrayBuffer
-        ).toString("base64")}`;
-      }
-
-      const workspace = await databases.createDocument(
-        DATABASE_ID,
-        WORKSPACES_ID,
-        ID.unique(),
-        {
-          name,
-          userId: user.$id,
-          imageUrl: uploadedImageUrl,
-        }
-      );
-
-      return c.json({ data: workspace });
-    } catch (error) {
-      if (error instanceof AppwriteException) {
-        if (error.code === 413) {
-          return c.json(
-            {
-              error:
-                "Payload is too large, please reduce containing files's size",
-            },
-            400
+      try {
+        if (image instanceof File) {
+          const file = await storage.createFile(
+            IMAGES_BUCKET_ID,
+            ID.unique(),
+            image
           );
-        }
-        return c.json({ error: error.message }, 400);
-      }
 
-      return c.json({ error: "Failed to create workspace" }, 500);
+          const arrayBuffer = await storage.getFilePreview(
+            IMAGES_BUCKET_ID,
+            file.$id
+          );
+
+          uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+            arrayBuffer
+          ).toString("base64")}`;
+        }
+
+        const workspace = await databases.createDocument(
+          DATABASE_ID,
+          WORKSPACES_ID,
+          ID.unique(),
+          {
+            name,
+            userId: user.$id,
+            imageUrl: uploadedImageUrl,
+          }
+        );
+
+        return c.json({ data: workspace });
+      } catch (error) {
+        if (error instanceof AppwriteException) {
+          if (error.code === 413) {
+            return c.json(
+              {
+                error:
+                  "Payload is too large, please reduce containing files's size",
+              },
+              400
+            );
+          }
+          return c.json({ error: error.message }, 400);
+        }
+
+        return c.json({ error: "Failed to create workspace" }, 500);
+      }
     }
-  }
-);
+  );
 
 export default app;
